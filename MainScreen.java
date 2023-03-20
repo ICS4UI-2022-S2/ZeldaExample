cecccf6cc02196909e35e4140ae00a4b36c28bc6
Adam Lamont <lamonta@gmail.com>
2023-03-19 21:18:21 -0400

    Initial Commit

---
+++ MainScreen.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+/**
+* Creates the main screen that displays everything
+* @author Mr. Lamont
+*/
+public class MainScreen extends JFrame implements ActionListener{
+
+
+
+  /**
+  * Constructor for the main screen
+  */
+  public MainScreen(){
+   
+    
+    
+  }
+  
+  
+  
+
+
+  /**
+  * Create and add all parts to the screen
+  */
+  private void initializeComponents(){
+  
+  }
+  
+
+
+
+  /** 
+  * The method called when a button is pressed
+  * @param e The actionEvent created from the button press
+  */
+  public void actionPerformed(ActionEvent e){
+    // determine the action command
+    String command = e.getActionCommand();
+   
+    
+
+  }
+
+
+
+
+}
